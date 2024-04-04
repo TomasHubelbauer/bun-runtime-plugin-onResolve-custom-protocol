f18997be5be07c4de0c82335fd3212738da65d60
guest271314 <guest271314@gmail.com>
2024-04-03 18:28:44 -0700

    Update plugin2.ts

--- plugin2.ts
+++ plugin2.ts
@@ -1,10 +1,21 @@
 import { plugin } from "bun";
 
 await plugin({
-  name: "test",
+  name: "test2",
   async setup(build) {
     console.log("Plugin 2 loaded!", build);
-
+    build.module(
+      // The specifier, which can be any string - except a built-in, such as "buffer"
+      "demo:2",
+      async () => {
+        const metaURL = new URL(await import.meta.resolve("./demo2.ts"), import.meta.url);
+        return {
+          contents: await (await fetch(metaURL.href)).text(),
+          loader: "ts",
+        };
+      }
+    )
+/*
     build.onResolve({ filter: /^demo:/ }, (args) => {
       console.log("onResolve2", args);
 
@@ -12,5 +23,6 @@ await plugin({
         path: './demo2.ts'
       };
     });
+*/
   },
 });
