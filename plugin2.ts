84e5b0373d686b41d250448c2b60f0867c4d1cda
guest271314 <guest271314@gmail.com>
2024-04-04 18:27:07 -0700

    Update plugin2.ts
    We parse the file that is going to be passed to `bun run` in the preloaded script, before we call `plugin()`. That is, we do what we want in the preload script outside of just relying on `bun.plugin()` to do the work. We use `RegExp` to match specifiers begining an arbitrary `specifier:` specifier, replace the exported content.

--- plugin2.ts
+++ plugin2.ts
@@ -1,16 +1,26 @@
+const url = new URL("./index.ts", import.meta.url);
+const script = await (await fetch(url.href)).text();
+const regexp = /(?<=['"])demo:\w+(?=['"])/g;
+const matches = script.match(regexp);
+
 import { plugin } from "bun";
 
-await plugin({
-  name: "test",
-  async setup(build) {
-    console.log("Plugin 2 loaded!", build);
-
-    build.onResolve({ filter: /^demo:/ }, (args) => {
-      console.log("onResolve2", args);
-
-      return {
-        path: './demo2.ts'
-      };
-    });
-  },
-});
+for (const specifier of matches) {
+  await plugin({
+    name: specifier,
+    async setup(build) {
+      console.log("Plugin 2 loaded!", build);
+      build.module(
+        // The specifier, which can be any string - except a built-in, such as "buffer"
+        specifier,
+        async () => {
+          console.log({ specifier });
+          return {
+            contents: `export default "Hi there, I am the '${specifier}' import!";`,
+            loader: "ts",
+          };
+        },
+      );
+    },
+  });
+}
